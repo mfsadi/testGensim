26fe8bfdc606ed0b78b7593bedda37d0c96561ff
mfsadi <31112688+mfsadi@users.noreply.github.com>
2017-09-04 15:09:46 +0000

    Github link added

--- testGensim.py
+++ testGensim.py
@@ -5,6 +5,7 @@ This script shows how to use gensim package in python for implementing word2vec 
 There is english and persian examples, but for achieving high quality word vectors it is strictly
 recommended to use big corpora as training data, like google news.
  ------M.F.Saadi, Zanjan University, Computer Department, 04/09/2017, mfsadi@znu.ac.ir----
+									https://github.com/mfsadi/testGensim
  Requirements:
   -gensim
   -matplotlib
